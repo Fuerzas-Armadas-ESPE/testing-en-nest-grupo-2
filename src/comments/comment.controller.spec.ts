c6782b666869fa2e386eb15996a0a3695a61537b
David Cantuña <edcantuna@espe.edu.ec>
2024-02-29 20:25:21 -0500

    Integracion del test

--- src/comments/comment.controller.spec.ts
+++ src/comments/comment.controller.spec.ts
@@ -1,27 +1,27 @@
-import { getModelToken } from "@nestjs/mongoose";
-import { Test, TestingModule } from "@nestjs/testing";
-import { CommentsController } from "./comment.controller";
-import { Comment } from "./comment.model";
-import { CommentsModule } from "./comment.module";
-import { CommentsService } from "./comment.service";
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+import { CommentsController } from './comment.controller';
+import { CommentsModule } from './comment.module';
+import { CommentsService } from './comment.service';
 
+// Este es el teste para los comentarios
 describe('CommentsController', () => {
-    let controller: CommentsController;
-    let service: CommentsService;
+  let controller: CommentsController;
+  let service: CommentsService;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            imports: [CommentsModule],
-        })
-            .overrideProvider(getModelToken('Comment'))
-            .useValue(jest.fn())
-            .compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [CommentsModule],
+    })
+      .overrideProvider(getModelToken('Comment'))
+      .useValue(jest.fn())
+      .compile();
 
-        controller = module.get<CommentsController>(CommentsController);
-        service = module.get<CommentsService>(CommentsService);
-    });
+    controller = module.get<CommentsController>(CommentsController);
+    service = module.get<CommentsService>(CommentsService);
+  });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
-      });
+  it('Debe estar definido', () => {
+    expect(service).toBeDefined();
+  });
 });
